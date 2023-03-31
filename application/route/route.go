f73294faa923deaf0c3655e7c140a1c520fb693e
Luvicitousr <luvicitousr@gmail.com>
2023-03-31 19:30:16 -0300

    commit with error

--- application/route/route.go
+++ application/route/route.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 	"strconv"
@@ -9,21 +10,21 @@ import (
 )
 
 type Route struct {
-	ID			string 'json:"routeID"'
-	ClientID 	string 'json:"clientId"'
-	Positions 	[]Position 'json:"position"'
+	ID        string     `json:"routeID"`
+	ClientID  string     `json:"clientId"`
+	Positions []Position `json:"position"`
 }
 
 type Position struct {
-	Lat float64 'json: lat'
-	Long float64 'json: long'
+	Lat  float64 `json: lat`
+	Long float64 `json: long`
 }
 
-type PartialRoutePosition struct{
-	ID 			string 		'json:"routeID"'
-	ClientID 	string 		'json:"clientId"'
-	Position 	[]float64 	'json:"position"'
-	Finished 	bool 		'json:"finished"'
+type PartialRoutePosition struct {
+	ID       string    `json:"routeID"`
+	ClientID string    `json:"clientId"`
+	Position []float64 `json:"position"`
+	Finished bool      `json:"finished"`
 }
 
 func (r *Route) LoadPositions() error {
@@ -40,7 +41,7 @@ func (r *Route) LoadPositions() error {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		data := string.Split(scanner.Text(), ",")
+		data := strings.Split(scanner.Text(), ",")
 		lat, err := strconv.ParseFloat(data[0], 64)
 		if err != nil {
 			return nil
@@ -50,16 +51,16 @@ func (r *Route) LoadPositions() error {
 			return nil
 		}
 		r.Positions = append(r.Positions, Position{
-			Lat: lat,
+			Lat:  lat,
 			Long: long,
 		})
-		
+
 	}
 	return nil
-	
+
 }
 
-func (r *Route) ExportJsonPositions() ([]string, error){
+func (r *Route) ExportJsonPositions() ([]string, error) {
 	var route PartialRoutePosition
 	var result []string
 	total := len(r.Positions)
@@ -68,7 +69,7 @@ func (r *Route) ExportJsonPositions() ([]string, error){
 		route.ClientID = r.ClientID
 		route.Position = []float64{v.Lat, v.Long}
 		route.Finished = false
-		if total - 1 == k{
+		if total-1 == k {
 			route.Finished = true
 		}
 		jsonRoute, err := json.Marshal(route)
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"fmt"
-	route2 "github.com/Luvicitousr/project-simulator/application/route"
+	"log"
+
+	"github.com/Luvicitousr/project-simulator/infra/kafka"
+	"github.com/joho/godotenv"
 )
 
-func main()  {
-	route := route2.Route{
-		ID: 		"1",
-		ClientID: 	"1",
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
-	route.LoadPositions()
-	stringjson := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
-	
+}
+
+func main() {
+	producer := kafka.NewKafkaProducer()
+	kafka.Publish("ola", "readtest", producer)
+
+	for {
+		_ = 1
+	}
+
+	// route := route2.Route{
+	// 	ID:       "1",
+	// 	ClientID: "1",
+	// }
+	// route.LoadPositions()
+	// stringjson, _ := route.ExportJsonPositions()
+	// fmt.Println(stringjson[1])
+
 }
